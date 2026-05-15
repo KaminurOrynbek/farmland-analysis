f3d0543ada817391fb3548534a60bbc59812470d
KassenkazyAlima <154450040+KassenkazyAlima@users.noreply.github.com>
2026-05-15 23:04:55 +0500

    padding fix

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -26,7 +26,7 @@ api.interceptors.response.use(
     if (!isAuthRequest && (error.response?.status === 401 || error.response?.status === 403)) {
       localStorage.removeItem('token');
       localStorage.removeItem('user');
-      window.location.href = '/login';
+      window.location.reload();
     }
 
     return Promise.reject(error);
