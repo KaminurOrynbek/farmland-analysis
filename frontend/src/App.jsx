fe56660d9958d04a10b2ab5d9bf40fadb4369116
KaminurOrynbek <153558671+KaminurOrynbek@users.noreply.github.com>
2026-06-02 21:54:52 +0500

    Remove import for AnalysisDetailsPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import Navbar from './components/layout/AppHeader';
 import ProjectsPage from './pages/ProjectsOverviewPage';
 import LandingPage from './pages/LandingPage';
-import AnalysisDetailsPage from './pages/FieldReportPage';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/DashboardPage';
 import ProfilePage from './pages/ProfilePage';
